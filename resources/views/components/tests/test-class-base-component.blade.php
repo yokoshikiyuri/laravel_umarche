e650a30afe3ebb5d1369bb824cfc6445e87db1fa
横式優里 <>
2022-01-03 21:18:36 +0900

    classBase

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,6 @@
+<div>
+    クラスベースのコンポーネントです
+    <div>{{ $classBaseMessage }}</div>
+    <div>{{ $defaultMessage }}</div>
+    <!-- Simplicity is the consequence of refined emotions. - Jean D'Alembert -->
+</div>
