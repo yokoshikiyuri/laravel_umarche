40442844d6997d67feff432143d7aa0cd06a5a1c
横式優里 <>
2022-01-03 16:53:36 +0900

    属性バッグ

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -3,4 +3,5 @@
   コンポーネントテスト1
   <x-tests.card title="タイトル" content="本文" :message="$message"/>
   <x-tests.card title="タイトル2"/>
+  <x-tests.card title="CSSを変更したい" class="bg-red-300"/>
 </x-tests.app>
