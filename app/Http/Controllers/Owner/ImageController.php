48dc36f3d3a746ef6921f6234497ba295dbad4f7
横式優里 <>
2022-04-14 10:54:01 +0900

    image index

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -4,9 +4,28 @@ namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Image;
+use Illuminate\Support\Facades\Auth;
 
 class ImageController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:owners');
+
+        $this->middleware(function ($request, $next) {
+            $id = $request->route()->parameter('image');
+            if (!is_null($id)) {
+                $imagesOwnerId = Image::findOrFail($id)->owner_id;
+                $imageId = (int)$imagesOwnerId;
+                if ($imageId !== Auth::id()) {
+                    abort(404);
+                }
+            }
+            return $next($request);
+        });
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +33,10 @@ class ImageController extends Controller
      */
     public function index()
     {
-        //
+        $images = Image::where('owner_id', Auth::id())
+            ->orderBy('updated_at', 'desc')
+            ->paginate(20);
+        return view('owner.images.index', compact('images'));
     }
 
     /**
